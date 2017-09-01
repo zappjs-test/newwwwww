14a44ac626b1ff3eae36f7cc9cedabe11337e9d5
zappjs-test <31462589+zappjs-test@users.noreply.github.com>
2017-09-01 03:45:34 -0500

    generates files

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,7 @@
-import webpack from 'webpack';
+const path = require('path');
+const webpack = require('webpack');
+
+const host = process.env.HOST || '0.0.0.0';
+const port = process.env.PORT || 3000;
+
+const env = process.env.NODE_ENV === 'production' ? 'production' : 'development';
